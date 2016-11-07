e9d0a024e8c330ead2690a40f0a8155823d4c347
web-developer-poletski <web.developer.poletski@gmail.com>
2016-11-07 12:03:59 +0300

    Add markdown support to module Landing

--- src/components/modules/Landing/Article.jsx
+++ src/components/modules/Landing/Article.jsx
@@ -6,10 +6,11 @@
  */
 import React from 'react';
 import ArticleLink from './ArticleLink';
+import marked from 'marked';
 
 export default React.createClass({
   displayName: 'Article',
-  
+
   propTypes: {
     className: React.PropTypes.string,
     title: React.PropTypes.string.isRequired,
@@ -17,12 +18,20 @@ export default React.createClass({
     link: React.PropTypes.objectOf(React.PropTypes.string)
   },
 
+  createMarkup(markupSource) {
+    return {
+      __html: marked(markupSource)
+    };
+  },
+
   render() {
     return (
       <div
-        className={this.props.className}>
+        className={this.props.className + ' article'}>
         <h3>{this.props.title}</h3>
-        <p className='lead'>{this.props.text}</p>
+        <div
+          dangerouslySetInnerHTML={this.createMarkup(this.props.text)}>
+        </div>
         <span className='article-link'>
           <ArticleLink {...this.props.link}/>
         </span>
